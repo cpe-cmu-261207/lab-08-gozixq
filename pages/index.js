1c85941eedecec7f9a8e5059385c7eba43baad0a
gozixq <natsinee2545@gmail.com>
2022-09-11 22:53:14 +0700

    completed

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,46 @@
-import ColorPickerContainer from "../components/ColorPickerContainer";
-import Header from "../components/Header";
-import { PainterContext } from "../contexts/PainterContext";
-import { useState } from "react";
-import Canvas from "../components/Canvas";
-import CanvasLib from "../libs/CanvasLib";
+const CanvasLib = {
+  createEmptyCanvas: () => {
+    const a = [];
+    for (let i = 0; i < 16; i++) {
+      a.push([]);
+      for (let j = 0; j < 16; j++) a[i].push("#FFFFFF"); //white color
+    }
+    return a;
+  },
 
-export default function Home() {
-  //selected color from color picker
-  //set black color as default
-  const [selColor, setSelColor] = useState("#000000");
+  copyCanvas: (source) => {
+    const a = [];
+    for (let i = 0; i < 16; i++) {
+      a.push([]);
+      for (let j = 0; j < 16; j++) a[i] = source[i];
+    }
+    return a;
+  },
 
-  //16x16 2D Array that holds color data
-  const [pixels, setPixels] = useState(CanvasLib.createEmptyCanvas());
+  createRandomCanvas: () => {
+    const colors = [
+      "#000000",
+      "#804000",
+      "#FE0000",
+      "#FE6A00",
+      "#FFD800",
+      "#00FF01",
+      "#FFFFFF",
+      "#01FFFF",
+      "#0094FE",
+      "#0026FF",
+      "#B100FE",
+      "#FF006E",
+    ];
 
-  //will be called by Cell component
-  const paint = (xPos, yPos) => {
-    //copy from old 2d Array
-    const newPixels = CanvasLib.copyCanvas(pixels);
-    //your code here
-  };
+    const b = [];
+    for (let i = 0; i < 16; i++) {
+      b.push([]);
+      for (let j = 0; j < 16; j++)
+        b[i].push(colors[Math.floor(Math.random() * 12)]); //white color
+    }
+    return b;
+  },
+};
 
-  const clear = () => {
-    //your code here
-    //Hint : use CanvasLib.createEmptyCanvas()
-  };
-
-  return (
-    <div style={{ minHeight: "100vh", backgroundColor: "GhostWhite" }}>
-      <PainterContext.Provider value={{ selColor, setSelColor, pixels, paint }}>
-        <Header />
-        <ColorPickerContainer />
-        <Canvas />
-
-        <div className="d-flex justify-content-center gap-2">
-          <button className="btn btn-dark" onClick={clear}>
-            Clear
-          </button>
-          <button className="btn btn-dark">Random Color</button>
-        </div>
-      </PainterContext.Provider>
-    </div>
-  );
-}
+export default CanvasLib;
